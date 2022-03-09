94d1776d682c7aa7879160b8beb4921b3407dd3e
Chloé Dorchêne <Chloe.Dorchene@grenoble-inp.org>
2022-03-09 09:27:36 +0100

    le nom des tags est ok, on peut cliquer sur new tag, masi il ne s'affiche pas encore

--- Exercice-50.js
+++ Exercice-50.js
@@ -3,18 +3,18 @@ const backend = "https://cawrest.ensimag.fr" // replace by the backend to use
 const token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiZG9yY2hlbmMiLCJkZWxlZyI6Im5vdC1kZWZpbmVkIn0.LwtktJF8T4CxqhmpHW97S9SMdoSHl3LXRy2xybAZ6Go" //replace by yout token : go to BACKEND/getjwsDeleg/caw to obtain it
 const wsBase = `${backend}/bmt/dorchenc` // replace USER by your login used tu obtain TOKEN
 const whoami = "/whoami"
-const bookmarks = "/bookmarks" 
+const bookmarks = "/bookmarks"
 const tags = "/tags"
 
 /* Shows the identity of the current user */
 function setIdentity() {
 	//TODO 1
-	let content = document.getElementsByClassName("identity")
+	const content = document.getElementsByClassName("identity")
 
 
-	fetch (backend + whoami, {method: 'GET', headers: {"x-access-token": token}})
-	.then(res => res.json())
-	.then(json => content.item(0).appendChild(document.createTextNode(json.data)))
+	fetch(backend + whoami, { method: 'GET', headers: { "x-access-token": token } })
+		.then(res => res.json())
+		.then(json => content.item(0).appendChild(document.createTextNode(json.data)))
 }
 
 /* Sets the height of <div id="#contents"> to benefit from all the remaining place on the page */
@@ -26,37 +26,37 @@ function setContentHeight() {
 	document.getElementById("contents").style.height = availableHeight + "px"
 }
 
-function changeSelected(ancienSelected, nouveauSelected){
-	var menu = document.getElementById("menu")
+// fonction qui enlève le selected de l'ancien (si il existe) et le met au nouveau
+function changeSelected(ancienSelected, nouveauSelected) {
+	const menu = document.getElementById("menu")
 	try {
 		menu.getElementsByClassName(ancienSelected + " selected")[0].className = ancienSelected
 	}
-	catch {/* dans ce cas là on ne fait rien car rien n'a été sélectionné */}
+	catch {/* dans ce cas là on ne fait rien car rien n'a été sélectionné */ }
 	menu.getElementsByClassName(nouveauSelected)[0].className = nouveauSelected + " selected"
 }
 
-
 /* Selects a new object type : either "bookmarks" or "tags" */
 function selectObjectType(type) {
-	var menu = document.getElementById("menu")
+	const menu = document.getElementById("menu")
 
 	// on trouve quel est le type selected et on enlève le selected pour le mettre sur l'autre type
-	var type_selected = null;
-	if (menu.getElementsByClassName("tags selected").length != 0){
+	const type_selected = null;
+	if (menu.getElementsByClassName("tags selected").length != 0) {
 		type_selected = "tags"
 	}
-	else if (menu.getElementsByClassName("bookmarks selected").length != 0){
+	else if (menu.getElementsByClassName("bookmarks selected").length != 0) {
 		type_selected = "bookmarks"
 	}
 
-	if (type != type_selected){
+	if (type != type_selected) {
 		// on fait une action que si le type sélectionné n'est pas le bon
-		if (type == "bookmarks"){
+		if (type == "bookmarks") {
 			changeSelected("tags", "bookmarks")
 			listBookmarks()
 			document.getElementById("add").getElementsByClassName("tag selected")[0].className = "tag"
 		}
-		else if (type == "tags"){
+		else if (type == "tags") {
 			changeSelected("bookmarks", "tags")
 			listTags()
 			document.getElementById("add").getElementsByClassName("tag")[0].className += " selected"
@@ -71,19 +71,28 @@ function listBookmarks() {
 	//TODO
 }
 
-function traiteListeTags(liste){
+// fonction qui retire tous les enfants d'un noeud
+function removeAll(node){
+	while (node.childNodes.length != 0) {
+		node.removeChild(node.firstChild)
+	}
+}
+
+/* fonction qui permet de traiter la liste de tags obtenue dans l'objet json d'un fetch */
+function traiteListeTags(liste) {
 	for (item in liste) {
 		// copie le model
 		let copie = document.getElementById("models").getElementsByClassName("model tag")[0].cloneNode(true)
 
 		// retire l'ancien nom et ajoute le nouveau
+		removeAll(copie.getElementsByTagName("h2")[0])
 		copie.getElementsByTagName("h2")[0].appendChild(document.createTextNode(liste[item].name))
-		
+
 		// change la classe de copie
 		copie.className = "item tag"
 
 		// TODO ajouter un nouvel attribut num
-		// copie.setAttribute(num, liste[item].id)
+		//copie.setAttribute(num, liste[item].id)
 
 		// ajoute à l'élément d'id items
 		console.log(document.getElementById("items"))
@@ -98,18 +107,35 @@ function listTags() {
 	console.log("listTags called")
 
 	// TODO on vide l'id items
-	// document.getElementById("items").remove()
+	removeAll(document.getElementById("items"))
 
 	// on va chercher dans l'url la liste de tous les tags
 	console.log(wsBase + tags)
-	fetch(wsBase + tags, {method: 'GET', headers: {"x-access-token": token}})
-	.then(res => res.json())
-	.then(json => {console.log(json.data); traiteListeTags(json.data)})
+	fetch(wsBase + tags, { method: 'GET', headers: { "x-access-token": token } })
+		.then(res => res.json())
+		.then(json => { console.log(json.data); traiteListeTags(json.data) })
 }
 
 /* Adds a new tag */
 function addTag() {
-	//TODO
+	const contenu = document.getElementsByTagName("input")[0].value
+	// TODO vérifier contenu
+	// assert(contenu != "")
+
+	// ajout du nouvel élément
+	const body = new URLSearchParams()
+	body.append("data", contenu)
+	fetch(
+		wsBase + tags,
+		{
+			method: 'POST',
+			headers: { "x-access-token": token },
+			body,
+		})
+
+	// affichage du nouvel élément + supression dans la barre de recherche
+	document.getElementsByTagName("input")[0].value = ""
+	listTags()
 }
 
 /* Handles the click on a tag */
@@ -133,13 +159,21 @@ function miseEnPlace() {
 	// Put the name of the current user into <h1>
 	setIdentity()
 
-	fetch( "https://cawrest.ensimag.fr/bmt/dorchenc/reinit", {method: 'POST', headers : {"x-access-token" : token}})
+	fetch("https://cawrest.ensimag.fr/bmt/dorchenc/reinit", { method: 'POST', headers: { "x-access-token": token } })
 	// Adapt the height of <div id="contents"> to the navigator window
 	setContentHeight()
-	window.addEventListener("resize",setContentHeight)
+	window.addEventListener("resize", setContentHeight)
 	// Listen to the clicks on menu items
 	// Initialize the object type to "tags"
 	selectObjectType("tags")
 	// Listen to clicks on the "add tag" button
 }
-window.addEventListener('load',miseEnPlace,false)
+
+window.addEventListener('load', miseEnPlace, false)
+
+window.onload = function () {
+	console.log(document.getElementById("addTag"))
+	document.getElementById("addTag").addEventListener("click", addTag, false)
+}
+
+
