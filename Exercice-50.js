936eebd769dc3d215ca392010250617472fca0d4
bossuetc <bossuet.celine@grenoble-inp.org>
2022-03-15 11:05:55 +0100

    je vous jure que ca marche maintenant

--- Exercice-50.js
+++ Exercice-50.js
@@ -1,10 +1,12 @@
 /* Base URL of the web-service for the current user and access token */
 const backend = "https://cawrest.ensimag.fr" // replace by the backend to use
 const token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiZG9yY2hlbmMiLCJkZWxlZyI6Im5vdC1kZWZpbmVkIn0.LwtktJF8T4CxqhmpHW97S9SMdoSHl3LXRy2xybAZ6Go" //replace by yout token : go to BACKEND/getjwsDeleg/caw to obtain it
-const wsBase = `${backend}/bmt/dorchenc` // replace USER by your login used tu obtain TOKEN
-const whoami = "/whoami"
-const bookmarks = "/bookmarks"
-const tags = "/tags"
+const wsBase = `${backend}/bmt/dorchenc/` // replace USER by your login used tu obtain TOKEN
+const bookmarks = "bookmarks"
+const tags = "tags"
+const selected = "selected"
+const itemTag = "item tag"
+const classListeCheckBoxTags = "classListeCheckboxTags"
 
 /* TODO : ce qu'il manque :
 	- ajouter un nouvel attribut num dans traiteListeTags au noeud copie DONE
@@ -15,10 +17,10 @@ const tags = "/tags"
 /* Shows the identity of the current user */
 function setIdentity() {
 	const content = document.getElementsByClassName("identity")
-
-	fetch(backend + whoami, { method: 'GET', headers: { "x-access-token": token } })
+	const urlw = "https://cawrest.ensimag.fr/whoami"
+	fetch(urlw, { headers: { "x-access-token": token } })
 		.then(res => res.json())
-		.then(json => content.item(0).appendChild(document.createTextNode(json.data)))
+		.then(json => {content[0].textContent=json['data'] })
 }
 
 /* Sets the height of <div id="#contents"> to benefit from all the remaining place on the page */
@@ -30,43 +32,37 @@ function setContentHeight() {
 	document.getElementById("contents").style.height = availableHeight + "px"
 }
 
-// fonction qui enlève le selected de l'ancien (si il existe) et le met au nouveau
-function changeSelected(ancienSelected, nouveauSelected) {
-	const menu = document.getElementById("menu")
-	try {
-		menu.getElementsByClassName(ancienSelected + " selected")[0].className = ancienSelected
-	}
-	catch {/* dans ce cas là on ne fait rien car rien n'a été sélectionné */ }
-	menu.getElementsByClassName(nouveauSelected)[0].className = nouveauSelected + " selected"
-}
 
 /* Selects a new object type : either "bookmarks" or "tags" */
 function selectObjectType(type) {
 	const menu = document.getElementById("menu")
+    const books = menu.children[0]
+    const tags = menu.children[1]
 
-	// on trouve quel est le type selected et on enlève le selected pour le mettre sur l'autre type
-	const type_selected = null;
-	if (menu.getElementsByClassName("tags selected").length != 0) {
-		type_selected = "tags"
-	}
-	else if (menu.getElementsByClassName("bookmarks selected").length != 0) {
-		type_selected = "bookmarks"
-	}
+    //si le type demandé est le type couramment affiché, rien ne se passe
+    if (books.className === "selected" && type === "bookmarks"){
+        return;
+    }
+    if (tags.className === "selected" && type === "tags"){
+        return;
+    }
 
-	if (type != type_selected) {
-		// on fait une action que si le type sélectionné n'est pas le bon
-		if (type == "bookmarks") {
-			changeSelected("tags", "bookmarks")
-			listBookmarks()
-			document.getElementById("add").getElementsByClassName("tag selected")[0].className = "tag"
-		}
-		else if (type == "tags") {
-			changeSelected("bookmarks", "tags")
-			listTags()
-			document.getElementById("add").getElementsByClassName("tag")[0].className += " selected"
-		}
-	}
-	// sinon on ne fait rien
+	//si le type demandé est bien bookmarks
+    if (type === "bookmarks"){
+        tags.className = "tags"
+        books.className = "selected"
+        listBookmarks()
+
+        // enlever la classe selected du <div class="tag"> qui se trouve dans le <div id="add">
+        document.getElementById("add").children[0].className = "tag"
+    } else if (type === "tags"){//le type demandé est tags
+            books.className = "bookmarks"
+            tags.className = "selected"
+            listTags()
+            document.getElementById("add").children[0].className = "selected"
+    }
+	//sinon on fait rien
+    
 }
 
 /* Loads the list of all bookmarks and displays them */
@@ -95,11 +91,9 @@ function traiteListeTags(liste) {
 		// change la classe de copie
 		copie.className = "item tag"
 		
-		
-		
 		// DONE ajouter un nouvel attribut num
-		let num;
-		copie.setAttribute(num, liste[item].id)
+		copie.setAttribute("num", liste[item].id)
+		console.log(copie)
 
 		// ajoute à l'élément d'id items
 		document.getElementById("items").appendChild(copie)
@@ -121,7 +115,40 @@ function listTags() {
 			headers: { "x-access-token": token }
 		})
 		.then(res => res.json())
-		.then(json => {traiteListeTags(json.data) })
+		.then(json => {
+			const obj = json
+			obj.data.forEach((element) => {
+				const copieNode = document.getElementsByClassName("model tag")[0].cloneNode(true)
+				//on ecrit dans h2 qui est le fils [0]
+				copieNode.children[0].textContent = element['name']
+				copieNode.setAttribute("num", element['id'])
+				copieNode.className = "item tag"
+				items.appendChild(copieNode)
+			});
+		})
+		.then(() => {
+			console.log("chargement click")
+			document.querySelectorAll(".item.tag").forEach(tag => {
+				tag.addEventListener('click', event => clickTag(tag), false)
+			})
+			console.log(document.querySelectorAll(".item.tag"))
+		})
+		.catch(error => {
+			console.log("errrr listTags + " + error.toString())
+		})
+}
+//requête client pour ajouter des Tag
+function postData(url, txt) {
+    return fetch(url, {
+        method: 'POST', 
+        "headers":
+                {
+                    "x-access-token": token,
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Accept": "application/json, text/plain, */*"
+                },
+        "body": `data={\"name\":\"${txt}\"}`
+    })
 }
 
 /* Adds a new tag */
@@ -134,19 +161,15 @@ function addTag() {
 	}
 	else {
 		// ajout du nouvel élément
-		// TODO à modifier
-		fetch(
-			wsBase + tags,
-			{
-				method: 'POST',
-				headers: { "x-access-token": token },
-				body: data={'name': contenu },
-			})
-
-		listTags()
-		// affichage du nouvel élément + supression dans la barre de recherche
-		document.getElementsByTagName("input")[0].value = ""
-		
+		const urltags = wsBase + "tags"
+    	console.log("ajout du " + contenu)
+    	postData(urltags, contenu)
+            .then(setTimeout(() => {
+                listTags()}, 500))
+				//pour afficher les modif après l'ajout et non pas avant. sans cette attente on ne voit pas les modif
+            .catch(error => {
+                console.log("errrr postData + " + error.toString())
+            })
 	}
 }
 
@@ -154,19 +177,107 @@ function addTag() {
 function clickTag(tag) {
 	console.log("clickTag")
 	console.log(tag)
-	if(document.getElementById("tag")[0].selected!=0){ //TODO condition
+	let oldSelectedItemTag = 0;
+    if (document.querySelector(".item.selected") != null) {
+        oldSelectedItemTag = document.querySelector(".item.selected")
+    }
+    tag.className = "item selected"
+    //cacher h2
+    tag.querySelector('h2').style.display = 'none'
+    if (oldSelectedItemTag === 0) {
+        const inputText = document.createElement("INPUT");
+        inputText.setAttribute("id", "line")
+        inputText.setAttribute("type", "text");
+        inputText.setAttribute("value", tag.querySelector('h2').textContent)
+        const modificationButton = document.createElement("INPUT");
+        modificationButton.setAttribute("id", "modification")
+        modificationButton.setAttribute("type", "button");
+        modificationButton.setAttribute("value", "modify name");
+        modificationButton.setAttribute("onclick", "modifyTag");
+        modificationButton.onclick = modifyTag
+        const suppressionnButton = document.createElement("INPUT");
+        suppressionnButton.setAttribute("id", "suppression")
+        suppressionnButton.setAttribute("type", "button");
+        suppressionnButton.setAttribute("value", "Remove tag");
+        suppressionnButton.setAttribute("onclick", "removeTag");
+        suppressionnButton.onclick = removeTag
 
-	}
+        tag.appendChild(inputText)
+        tag.appendChild(modificationButton)
+        tag.appendChild(suppressionnButton)
+
+    } 
+    else if (tag != oldSelectedItemTag) 
+    {
+        oldSelectedItemTag.className = "item tag"
+        oldSelectedItemTag.querySelector('h2').style.display = 'initial'
+        console.log(tag.querySelector('h2').textContent)
+        tag.appendChild(document.getElementById("line"))
+        document.getElementById("line").value = tag.querySelector('h2').textContent
+        tag.appendChild(document.getElementById("modification"))
+        tag.appendChild(document.getElementById("suppression"))
+    }
+}
+function putData(url, txt) {
+    // Default options are marked with * 
+    return fetch(url, {
+        method: 'PUT',
+        "headers":
+                {
+                    "x-access-token": token,
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Accept": "application/json, text/plain, */*"
+                },
+        "body": `data={"name":"${txt}"}`
+                //body: new URLSearchParam({"data": JSON.stringify({"name": input.value))
+    })
 }
 
 /* Performs the modification of a tag */
 function modifyTag() {
-	//TODO 8
+	const tagDiv = document.querySelector(".item.selected")
+    const id = tagDiv.attributes['num'].nodeValue
+    const url = wsBase + "tags/" + id
+
+    const line = document.getElementById("line")
+    const txt = line.value
+
+    putData(url, txt)
+            .then(setTimeout(() => {
+                listTags()
+            }, 500))//pour afficher les modif après l'ajout et non pas avant. sans cette attente on ne voit pas les modif
+            .catch(error => {
+                console.log("error modifyTag + " + error.toString())
+            })
+}
+
+function removeData(url, id) {
+    // Default options are marked with *
+    return fetch(url + "/" + id, {
+        method: 'DELETE',
+        "headers":
+                {
+                    "x-access-token": token,
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "Accept": "application/json, text/plain, */*"
+                },
+        //"body": `data={\"name\":\"${id}\"}`
+    })
 }
 
 /* Removes a tag */
 function removeTag() {
-	//TODO 9
+	const url = wsBase + "tags/"
+    const tagDiv = document.querySelector(".item.selected")
+    const id = tagDiv.attributes['num'].nodeValue
+
+    removeData(url, id)
+            .then(setTimeout(() => {
+                listTags()
+            }, 500))//pour afficher les modif après l'ajout et non pas avant. sans cette attente on ne voit pas les modif
+            .catch(error => {
+                console.log("error removeTag + " + error.toString())
+            })
 }
 /* On document loading */
 function miseEnPlace() {
@@ -179,9 +290,19 @@ function miseEnPlace() {
 	setContentHeight()
 	window.addEventListener("resize", setContentHeight)
 	// Listen to the clicks on menu items
+	const menu = document.getElementById("menu")
+    const books = menu.children[0]
+    const tags = menu.children[1]
+    books.addEventListener('click', function () {
+        selectObjectType("bookmarks")
+    })
+    tags.addEventListener('click', function () {
+        selectObjectType("tags")
+    })
 	// Initialize the object type to "tags"
 	selectObjectType("tags")
 	// Listen to clicks on the "add tag" button
+	document.getElementById('addTag').addEventListener('click', addTag)
 }
 
 window.addEventListener('load', miseEnPlace, false)
